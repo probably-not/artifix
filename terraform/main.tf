63fbc4666c2a45c5bed6060f234fb9cb432a4387
Coby Benveniste <coby.benveniste@gmail.com>
2025-01-19 17:27:33 +0200

    Set strict versions on the terraform setup

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "5.84.0"
     }
   }
 
   backend "s3" {}
 
-  required_version = "~> 1.7"
+  required_version = "1.10.4"
 }
