cf0173172082d5f4b816c963246b621977a683b7
Coby Benveniste <coby.benveniste@gmail.com>
2025-01-20 11:24:54 +0200

    Add route53 record for registry

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -2,3 +2,16 @@ data "aws_route53_zone" "registry" {
   name         = var.hosted_zone_name
   private_zone = false
 }
+
+
+resource "aws_route53_record" "registry" {
+  zone_id = data.aws_route53_zone.registry.zone_id
+  name    = var.registry_domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.registry.domain_name
+    zone_id                = aws_cloudfront_distribution.registry.hosted_zone_id
+    evaluate_target_health = true
+  }
+}
